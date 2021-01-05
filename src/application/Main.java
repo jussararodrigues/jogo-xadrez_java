525ecde0ea96a80bf831b577349d115a6a667d6f
Jussara Rodrigues <jussara.rodrigues10@hotmail.com>
2021-01-04 23:51:27 -0300

    Adiciona a primeira classe: Posicao

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Posicao;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Posicao posicao = new Posicao(3, 5);
 		
+		System.out.println(posicao);
 	}
 	
 }
